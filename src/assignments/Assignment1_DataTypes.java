aea59c468bf55db1d5e8b077552d31e849d13049
Latif Inamdar <latifafajalinamdar@gmail.com>
2025-08-08 21:33:42 +0530

    Assignment 1

--- src/assignments/Assignment1_DataTypes.java
+++ src/assignments/Assignment1_DataTypes.java
@@ -6,7 +6,12 @@ public class Assignment1_DataTypes {
 	{
 		//Temperature of a city in degrees Celsius: 25.5
 		double temp = 25.5;
-		System.out.println("Temperature of a city in degrees Celsius" +temp);
+		System.out.println("Temperature of a city in degrees Celsius " +temp);
+		
+		//Whether a customer has placed an order: true or false
+		
+		boolean order_placed=true;
+		System.out.println("Whether a customer has placed an order: true or false "+order_placed);
 
 	}
 
