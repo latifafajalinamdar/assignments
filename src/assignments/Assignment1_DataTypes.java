4c75277a2c18d92ffdf1a0151fe267ae20e5d103
Latif Inamdar <latifafajalinamdar@gmail.com>
2025-08-08 21:28:14 +0530

    Assignment 1

--- src/assignments/Assignment1_DataTypes.java
+++ src/assignments/Assignment1_DataTypes.java
@@ -2,8 +2,11 @@ package assignments;
 
 public class Assignment1_DataTypes {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		//Temperature of a city in degrees Celsius: 25.5
+		double temp = 25.5;
+		System.out.println("Temperature of a city in degrees Celsius" +temp);
 
 	}
 
