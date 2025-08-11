9608a4d409c96c1159deff0396321c69aaec968a
Latif Inamdar <latifafajalinamdar@gmail.com>
2025-08-11 20:26:02 +0530

    Assignment1 Pending

--- src/assignments/Assignment1_DataTypes.java
+++ src/assignments/Assignment1_DataTypes.java
@@ -11,8 +11,83 @@ public class Assignment1_DataTypes {
 		//Whether a customer has placed an order: true or false
 		
 		boolean order_placed=true;
-		System.out.println("Whether a customer has placed an order: true or false "+order_placed);
+		System.out.println("Whether a customer has placed an order: "+order_placed);
+		
+		//Person's phone number: "123-456-7890"
+		
+		String phone_number = "123-456-7890";
+		System.out.println("Person's phone number: "+phone_number);
+		
+		//Amount of money in a customer's bank account: 1000.50
+		double amount_bankaccount = 1000.50;
+		System.out.println("Amount of money in a customer's bank account "+amount_bankaccount);
+		
+		//Person's email address: "john.doe@example.com"
+		String email_address = "john.doe@example.com";
+		System.out.println("Person's email address"+email_address);
+		
+		//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		double coordinates_of_location_latitude=37.7749;
+		double coordinates_of_location_longitude=-122.4194;
+		System.out.print("Coordinates of a location latitude longitude"+coordinates_of_location_latitude);
+		System.out.println("Coordinates of a location latitude longitude"+coordinates_of_location_longitude);
+		
+		//Person's marital status: true or false
+		boolean persons_marital_status = true;
+		System.out.println("Person's marital status"+persons_marital_status);
+		
+		//Person's occupation: "Software Engineer"
+		String personsoccupation = "Software Engineer";
+		System.out.println("Person's occupation "+personsoccupation);
+		
+		//Person's favourite colour: "Blue"
+		
+		String personFavourite_Colour = "Blue";
+		System.out.println("Person's favourite colour"+personFavourite_Colour);
+		
+		//Number of employees in a company: 500
+		int numberOfEmployCompany= 500 ;
+		System.out.println("Number of employees in a company"+numberOfEmployCompany) ;
+				
+				//Time of an event: 2:30 PM
+		double timeOfEvent = 2.30;
+		System.out.println("Time of an event"+timeOfEvent+"PM");
+				
+				//Name of a country: "United States"
+		String nameOfCountry = "United States";
+		System.out.println("Name of a country:"+nameOfCountry);
+					
+				//Person's eye color: "Brown"
+		String personEyeColor = "Brown";
+		System.out.println("Person's eye color:"+personEyeColor);
+				
+				//Person's birthplace: "New York City"
+		String personBirthPlace = "New York City";
+		System.out.println("Person's birthplace:"+personBirthPlace);
 
+		
+		//Current year: 2023
+		int current_year = 2025;
+		System.out.println("Current year "+current_year);
+		
+		//Number of followers on a social media platform: 1,000,000
+		int number_Of_Followers_On_Social_Media_Platform = 1000000;
+		System.out.println("Number of followers on a social media platform: "+number_Of_Followers_On_Social_Media_Platform);
+		
+		//Rating of a movie: 7.5
+		double Rating_OfMovie = 7.5;
+		System.out.println("Rating of a movie: "+Rating_OfMovie);
+		
+		//Person's blood type: 'A'
+		char person_Blood_Type = 'A';
+		System.out.println("Person's blood type:"+person_Blood_Type);
+				
+		//Title of a book: "To Kill a Mockingbird"
+		String title_Of_Book = "To Kill a Mockingbird";
+		System.out.println("Title of a book"+title_Of_Book);
+		
+		
+		
 	}
 
 }
---
+++ src/assignments/Assignment2_Arrays.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class Assignment2_Arrays {
+
+	public static void main(String[] args) 
+	{
+		String[] emp= {"abc","pqr"};
+		for(int i=0;i<emp.length;i++)
+		{
+		System.out.println("employee" [i] "Details are :"+emp[i]);
+
+	}
+
+}
+}
