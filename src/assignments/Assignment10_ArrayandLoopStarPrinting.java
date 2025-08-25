8e42bd6916ed0b2228202e04dc593783fe980456
Latif Inamdar <latifafajalinamdar@gmail.com>
2025-08-25 21:13:45 +0530

    Assignments 9,10,11

---
+++ src/assignments/Assignment10_ArrayandLoopStarPrinting.java
@@ -0,0 +1,27 @@
+package assignments;
+
+public class Assignment10_ArrayandLoopStarPrinting {
+
+	public static void main(String[] args) 
+	{
+		 for (int i = 1; i <= 5; i++) {
+	            for (int j = i; j >= 1; j--) {
+	                System.out.print(j + " ");
+	            }
+	            System.out.println();
+	        } 
+		
+		
+		for (int i = 4; i >= 1; i--) {
+	            for (int j = 1; j <= i; j++) {
+	                System.out.print(j + " ");
+	            }
+	            System.out.println();
+	        }
+    }
+	
+	
+	
+}
+
+
---
+++ src/assignments/Assignment10_ArraysandLoops.java
@@ -0,0 +1,51 @@
+package assignments;
+
+public class Assignment10_ArraysandLoops {
+
+	public static void main(String[] args) 
+	{
+		int[] arr= {12 ,34,11,36,87,98,93};
+		
+		int largest=-1;
+		int secondlargets=-1;
+		int thirdlargest=-1;
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			
+			if(arr[i]>largest)
+			{
+				largest=arr[i];
+			}
+		}
+		
+		System.out.println("Largest Number is "+largest);
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			
+			if(arr[i]>thirdlargest &&(arr[i]!=largest))
+				
+			{
+				secondlargets=arr[i];
+			}
+		}
+
+		
+		System.out.println("Second Largest="+secondlargets);
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			
+			if(arr[i]>thirdlargest &&(arr[i]!=largest)&&(arr[i]!=secondlargets))
+				
+			{
+				thirdlargest=arr[i];
+			}
+		}
+
+		System.out.println("Third Largest "+thirdlargest);
+		
+	}
+
+}
